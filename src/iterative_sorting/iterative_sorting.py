d9d52e1de25e51600a8d8c8ee80d63dd2f548827
Melvin Merlos <melvinamerlos@gmail.com>
2019-08-24 14:29:37 -0400

    mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,22 +6,47 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
 
-
+# Loop through elements on right-hand-side of current index 
+        for j in range(cur_index, len(arr)):
+            rhs_cur_index = j
+             # if (the value of the smallest_index is more than value of rhs_cur_index):
+                # smallest value location is the rhs_cur_index at iteration j
+            if arr[smallest_index] > arr[rhs_cur_index]:
+                smallest_index = rhs_cur_index
 
         # TO-DO: swap
+        # placeholder holds the smallest element in iteration i
+        placeholder = arr[smallest_index]
 
+        # swap the number at iteration j with number at iteration i
+        arr[smallest_index] = arr[cur_index]
 
+        # and vice-versa
+        arr[cur_index] = placeholder
 
-
+    print(arr)
     return arr
 
+arr = [2,3,4,8,5]
+selection_sort( arr )
 
-# TO-DO:  implement the Bubble Sort function below
+
+# TO-DO: implement the Bubble Sort function below
+
 def bubble_sort( arr ):
-
+    # Loop through your array
+    for i in range( 0, len( arr )-1 ):
+        # Compare each element to its neighbor
+        for j in range( 0, len( arr )-i-1 ):
+            # If elements in wrong position (relative to each other, swap them)
+            if ( arr[j] > arr[j+1] ):
+                # The swap
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    print(arr)
     return arr
+
+bubble_sort(arr)
 
 
 # STRETCH: implement the Count Sort function below
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,17 +3,62 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    a, b, i = 0, 0, 0 # counters for each array
+    # while counter, 'a' AND 'b' is less than length of 'arrA' AND 'arrB', respectively do the following...
+    while a < len(arrA) and b < len(arrB):
+        # if value of item at index 'a' in 'arrA' is less than or equal to the value of the item in index 'b' of 'arrB'
+        if arrA[a] <= arrB[b]:
+            # then take the value that's in 'arrA', index 'a' and replace value of index 'i' in 'merged_arr'
+            merged_arr[i] = arrA[a]
+            a += 1 # and increment 'a'
+        # otherwise if value of item at index 'a' in 'arrA' is NOT less than or equal to the value of the item in index 'b' of 'arrB'
+        else:
+            # then take the value that's in 'arrB', index 'b' and replace value of index 'i' in 'merged_arr'
+            merged_arr[i] = arrB[b]
+            b += 1 # and increment 'b'
+        # increment i
+        i += 1
+
+    # while counter 'a' is less than length of 'arrA' do the following...
+    while a < len(arrA):
+        # replace value of index 'i' of 'merged_arr' with value of index 'a' in 'arrA'
+        merged_arr[i] = arrA[a]
+        a += 1 # increment
+        i += 1
+    # while counter 'b' is less than length of 'arrB' do the following...
+    while b < len(arrB):
+        # replace value of index 'i' of 'merged_arr' with value of index 'b' in 'arrB'
+        merged_arr[i] = arrB[b]
+        b += 1 # increment
+        i += 1
+
+    print("merge function:", merged_arr)
     return merged_arr
 
+arr = [2, 3, 4, 8, 5]
+arr2 = [22, 44, 88, 66, 88]
+
+merge( arr, arr2 )
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # if length of input is anything greater than 1..
+    if len(arr) > 1:
+        # determine midpoint
+        mid = int(len(arr) / 2)
+        # call recursively to split using midpoint
+        lhs = merge_sort(arr[:mid])
+        rhs = merge_sort(arr[mid:])
+        # call merge(lhs,rhs) defined above
+        arr = merge(lhs, rhs)
 
+    print("merge_sort function:", arr)
     return arr
 
+merge_sort( arr )
 
+###########################################
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
